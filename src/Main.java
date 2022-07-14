6a03f89b0510149fe6b5339c2eab08294c3e8ced
Анна Станкевич <stankevich.anna.94@mail.ru>
2022-07-14 20:32:48 +0300

    add_java-hw-2.1.iml_&_src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int rublePerMile = 20;
+
+        int bonusMiles = ticketPrice / rublePerMile;
+
+        System.out.println((bonusMiles) + " бонусных(ая) миль(я)");
+    }
+
+}
